bc2ac0f4d96caf5e95ed638e84294f6b5808fd20
serialbandicoot <samtreweek@gmail.com>
2024-08-22 12:55:59 +0100

    adjust paths

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
-// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@test/(.*)$': '<rootDir>/test/$1'
+    '^@test/(.*)$': '<rootDir>/src/test/$1' // Adjusted to match your tsconfig paths
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src', '<rootDir>/test'], // Ensure Jest knows where to look
+  roots: ['<rootDir>/src/test'], // Corrected this line
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'], // Match your test files
   clearMocks: true,
 };
