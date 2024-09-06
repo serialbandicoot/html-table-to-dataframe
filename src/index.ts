9ef0bad3a977898d2d68d3aede70439fa77bc139
serialbandicoot <samtreweek@gmail.com>
2024-09-06 18:39:55 +0100

    make interactable available

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { toDataFrame } from './table-data';
 import { toPrettyPrint } from './table-pretty';
+import { toInteractiveDataFrame } from './table-interactive';
 
-export { toPrettyPrint, toDataFrame };
+export { toPrettyPrint, toDataFrame, toInteractiveDataFrame };
 export * from './table-to-have';
