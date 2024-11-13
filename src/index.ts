a109201caa0078ca07a11fc2de9131d90c562d8b
serialbandicoot <samtreweek@gmail.com>
2024-11-13 13:52:16 +0000

    split out frames to separate classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './table-to-have';
-export { DataFrameOptions } from './data-frame';
-export { toDataFrame } from './table-data';
+export { DataFrameOptions } from './base-frame';
+export { toDataFrame, toInteractiveDataFrame } from './table-data';
 export { toPrettyPrint } from './table-pretty';
-export { toInteractiveDataFrame } from './table-interactive';
 export { DataFrame, LocatorID, Attributes, TableData } from './types';
