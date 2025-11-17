6585d4b29ca562916a88f4262efc761f4835917f
cgi-sam <sam.treweek@cgi.com>
2025-11-17 17:19:47 +0000

    update

--- src/test/to-interactive.spec.ts
+++ src/test/to-interactive.spec.ts
@@ -13,141 +13,140 @@ test('should get an interactive table', async () => {
     {
       Person: {
         attributes: {
-          id: "person-1",
-          name: "person-1",
-          "data-test-id": "person-chris",
+          id: 'person-1',
+          name: 'person-1',
+          'data-test-id': 'person-chris',
         },
-        type: "select",
+        type: 'select',
       },
       Likes: {
         attributes: {
-          id: "likes-1",
-          name: "likes-1",
-          "data-test-id": "likes-chris",
+          id: 'likes-1',
+          name: 'likes-1',
+          'data-test-id': 'likes-chris',
         },
-        type: "textarea",
+        type: 'textarea',
       },
       Age: {
         attributes: {
-          type: "number",
-          id: "age-1",
-          name: "age-1",
-          value: "22",
-          "data-testid": "age-1",
+          type: 'number',
+          id: 'age-1',
+          name: 'age-1',
+          value: '22',
+          'data-testid': 'age-1',
         },
-        type: "input",
+        type: 'input',
       },
       Links: {
         attributes: {
-          href: "#chris-1",
+          href: '#chris-1',
         },
-        type: "link",
+        type: 'link',
       },
     },
     {
       Person: {
         attributes: {
-          id: "person-2",
-          name: "person-2",
-          "data-test-id": "person-dennis",
+          id: 'person-2',
+          name: 'person-2',
+          'data-test-id': 'person-dennis',
         },
-        type: "select",
+        type: 'select',
       },
       Likes: {
         attributes: {
-          id: "likes-2",
-          name: "likes-2",
-          "data-test-id": "likes-dennis",
+          id: 'likes-2',
+          name: 'likes-2',
+          'data-test-id': 'likes-dennis',
         },
-        type: "textarea",
+        type: 'textarea',
       },
       Age: {
         attributes: {
-          type: "number",
-          id: "age-2",
-          name: "age-2",
-          value: "45",
-          "data-testid": "age-2",
+          type: 'number',
+          id: 'age-2',
+          name: 'age-2',
+          value: '45',
+          'data-testid': 'age-2',
         },
-        type: "input",
+        type: 'input',
       },
       Links: {
         attributes: {
-          href: "#dennis-2",
+          href: '#dennis-2',
         },
-        type: "link",
+        type: 'link',
       },
     },
     {
       Person: {
         attributes: {
-          id: "person-3",
-          name: "person-3",
-          "data-test-id": "person-sarah",
+          id: 'person-3',
+          name: 'person-3',
+          'data-test-id': 'person-sarah',
         },
-        type: "select",
+        type: 'select',
       },
       Likes: {
         attributes: {
-          id: "likes-3",
-          name: "likes-3",
-          "data-test-id": "likes-sarah",
+          id: 'likes-3',
+          name: 'likes-3',
+          'data-test-id': 'likes-sarah',
         },
-        type: "textarea",
+        type: 'textarea',
       },
       Age: {
         attributes: {
-          type: "number",
-          id: "age-3",
-          name: "age-3",
-          value: "29",
-          "data-testid": "age-3",
+          type: 'number',
+          id: 'age-3',
+          name: 'age-3',
+          value: '29',
+          'data-testid': 'age-3',
         },
-        type: "input",
+        type: 'input',
       },
       Links: {
         attributes: {
-          href: "#sarah-3",
+          href: '#sarah-3',
         },
-        type: "link",
+        type: 'link',
       },
     },
     {
       Person: {
         attributes: {
-          id: "person-4",
-          name: "person-4",
-          "data-test-id": "person-karen",
+          id: 'person-4',
+          name: 'person-4',
+          'data-test-id': 'person-karen',
         },
-        type: "select",
+        type: 'select',
       },
       Likes: {
         attributes: {
-          id: "likes-4",
-          name: "likes-4",
-          "data-test-id": "likes-karen",
+          id: 'likes-4',
+          name: 'likes-4',
+          'data-test-id': 'likes-karen',
         },
-        type: "textarea",
+        type: 'textarea',
       },
       Age: {
         attributes: {
-          type: "number",
-          id: "age-4",
-          name: "age-4",
-          value: "36",
-          "data-testid": "age-4",
+          type: 'number',
+          id: 'age-4',
+          name: 'age-4',
+          value: '36',
+          'data-testid': 'age-4',
         },
-        type: "input",
+        type: 'input',
       },
       Links: {
         attributes: {
-          href: "#karen-4",
+          href: '#karen-4',
         },
-        type: "link",
+        type: 'link',
       },
     },
-  ]
-  );
+  ]);
 });
 
 test('should get an interactive table', async () => {
@@ -158,28 +157,28 @@ test('should get an interactive table', async () => {
   const dataFrame = toDataFrame(htmlString);
   const expected = [
     {
-      Person: "Chris",
-      Likes: "HTML tables",
-      Age: "22",
-      Links: "chris-1",
+      Person: 'Chris',
+      Likes: 'HTML tables',
+      Age: '22',
+      Links: 'chris-1',
     },
     {
-      Person: "Dennis",
-      Likes: "Web accessibility",
-      Age: "45",
-      Links: "dennis-2",
+      Person: 'Dennis',
+      Likes: 'Web accessibility',
+      Age: '45',
+      Links: 'dennis-2',
     },
     {
-      Person: "Sarah",
-      Likes: "JavaScript frameworks",
-      Age: "29",
-      Links: "sarah-3",
+      Person: 'Sarah',
+      Likes: 'JavaScript frameworks',
+      Age: '29',
+      Links: 'sarah-3',
     },
     {
-      Person: "Karen",
-      Likes: "Web performance",
-      Age: "36",
-      Links: "karen-4",
+      Person: 'Karen',
+      Likes: 'Web performance',
+      Age: '36',
+      Links: 'karen-4',
     },
   ];
   expect(dataFrame).toEqual(expected);
